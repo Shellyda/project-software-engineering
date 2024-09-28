282174489a67f65ffed142ce3ae7f46507c9492a
josebasilioo <josebasiliosilvaneto@gmail.com>
2024-09-28 15:25:55 -0300

    fix: removed component from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,8 @@
-import Slider from '@/components/atoms/Slider';
-import ReceipeCard from '@/components/molecules/ReceipeCard';
 import { BaseLayout } from '@/components/templates/BaseLayout';
 
 const HomePage = () => (
   <div>
     <BaseLayout>
-      <Slider>
-        <ReceipeCard
-          title="Bolo de rolo"
-          image="https://plus.unsplash.com/premium_photo-1690214491960-d447e38d0bd0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D"
-          initialRating={3}
-          receipeId="1"
-        />
-        <ReceipeCard
-          title="Bolo de rolo"
-          image="https://plus.unsplash.com/premium_photo-1690214491960-d447e38d0bd0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D"
-          initialRating={3}
-          receipeId="1"
-        />
-        <ReceipeCard
-          title="Bolo de rolo"
-          image="https://plus.unsplash.com/premium_photo-1690214491960-d447e38d0bd0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D"
-          initialRating={3}
-          receipeId="1"
-        />
-        <ReceipeCard
-          title="Bolo de rolo"
-          image="https://plus.unsplash.com/premium_photo-1690214491960-d447e38d0bd0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D"
-          initialRating={3}
-          receipeId="1"
-        />
-      </Slider>
       <h1>Home</h1>
     </BaseLayout>
   </div>
