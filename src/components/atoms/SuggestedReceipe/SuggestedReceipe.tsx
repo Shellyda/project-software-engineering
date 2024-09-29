16a3ef52e801bce0be2a65a9bd54c9e8f1aae391
josebasilioo <josebasiliosilvaneto@gmail.com>
2024-09-29 20:23:12 -0300

    fix: added stars to suggested recipe card

--- src/components/atoms/SuggestedReceipe/SuggestedReceipe.tsx
+++ src/components/atoms/SuggestedReceipe/SuggestedReceipe.tsx
@@ -1,10 +1,13 @@
 import { Pacifico } from 'next/font/google';
 import React from 'react';
 
+import StarRating from '@/components/organisms/StarRating';
+
 interface SuggedtedRecipeProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
   subtitle: string;
   time: number;
+  initialRating: number;
 }
 
 const pacifico = Pacifico({
@@ -16,6 +19,7 @@ const SuggedtedRecipe: React.FC<SuggedtedRecipeProps> = ({
   title,
   subtitle,
   time,
+  initialRating,
   ...divProps
 }) => {
   return (
@@ -24,7 +28,14 @@ const SuggedtedRecipe: React.FC<SuggedtedRecipeProps> = ({
       className={`flex items-center bg-base-beige rounded-md py-2 px-4 max-w-sm shadow-md`}
     >
       <div className="flex flex-col">
-        <h2 className={`text-xl font-bold text-secondary-base ${pacifico.className}`}>{title}</h2>
+        <div className="flex flex-row items-center">
+          <h2 className={`text-xl font-bold text-secondary-base ${pacifico.className}`}>{title}</h2>
+          <StarRating
+            initialRating={initialRating}
+            disabled
+            style={{ width: '30%', marginLeft: '1rem' }}
+          />
+        </div>
         <p className="text-light-secondary-base">
           {subtitle} - {time} min
         </p>
