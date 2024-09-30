3bc9bdb92d2d3c06f85afc4728d3aeb6f56287a1
shellyda.de.barbosa <shellydabarbosa@gmail.com>
2024-09-30 13:25:56 -0300

    chore: installing and configuring chakra ui

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  primary: '#FCD74F',
+  secondary: '#136640',
+  base: '#F4F0E0',
+  'secondary-base': '#3D2D1D',
+  'black-primary': '#2E2C25',
+  'base-beige': '#CBB9A7',
+  'light-secondary-base': '#7B4D1F'
+};
+
+export default colors;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,17 @@
+import colors from './src/styles/colors';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        primary: ['var(--font-primary)']
-      },
-      colors: {
-        primary: '#FCD74F',
-        secondary: '#136640',
-        base: '#F4F0E0',
-        'secondary-base': '#3D2D1D',
-        'black-primary': '#2E2C25',
-        'base-beige': '#CBB9A7',
-        'light-secondary-base': '#7B4D1F'
-      }
-    }
-  },
-  variants: {
-    extend: {}
-  },
-  plugins: [require('tailwind-scrollbar')]
+export const mode = 'jit';
+export const content = ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {
+    fontFamily: {
+      primary: ['var(--font-primary)']
+    },
+    colors
+  }
 };
+export const variants = {
+  extend: {}
+};
+export const plugins = [require('tailwind-scrollbar')];
