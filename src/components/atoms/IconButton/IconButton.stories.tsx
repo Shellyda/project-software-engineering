425d2a30256c78a0eedf000d93e3f4ef4bba1957
shellyda.de.barbosa <shellydabarbosa@gmail.com>
2024-10-01 02:23:11 -0300

    fix: storybook

--- src/components/atoms/IconButton/IconButton.stories.tsx
+++ src/components/atoms/IconButton/IconButton.stories.tsx
@@ -1,43 +1,20 @@
-// IconButton.stories.tsx
-
-import { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import IconButton from './IconButton';
-import HomeOutlineIcon from './icons/HomeOutlineIcon.svg';
-import HomeSolidIcon from './icons/HomeSolidIcon.svg';
-import { IconButtonProps } from './interfaces';
 
-const meta: Meta<IconButtonProps> = {
-  title: 'Components/IconButton',
+const meta: Meta<typeof IconButton> = {
+  title: 'Atoms/IconButton',
   component: IconButton,
-  argTypes: {
-    isActive: {
-      control: { type: 'boolean' }
-    },
-    onClick: { action: 'clicked' }
+  tags: ['autodocs'],
+  parameters: {
+    nextjs: {
+      appDirectory: true
+    }
   }
 };
 
 export default meta;
 
-const Template: StoryFn<IconButtonProps> = (args: IconButtonProps) => <IconButton {...args} />;
+type Story = StoryObj<typeof IconButton>;
 
-export const Outline = Template.bind({});
-Outline.args = {
-  isActive: false,
-  outlineIcon: HomeOutlineIcon,
-  solidIcon: HomeSolidIcon,
-  onClick: () => {},
-  width: 32,
-  height: 32
-};
-
-export const Solid = Template.bind({});
-Solid.args = {
-  isActive: true,
-  outlineIcon: HomeOutlineIcon,
-  solidIcon: HomeSolidIcon,
-  onClick: () => {},
-  width: 32,
-  height: 32
-};
+export const Default: Story = {};
