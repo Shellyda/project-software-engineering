d7a49d4b1a04acecc5bf895ef8c3d461d56b9d02
josebasilioo <josebasiliosilvaneto@gmail.com>
2024-10-02 15:59:39 -0300

    fix: lint

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -62,11 +62,7 @@ const Search = () => {
 
   return (
     <BaseLayout>
-      <Greeting
-        title="Encontre"
-        isAuthenticated={!!user?.id}
-        userImage="https://plus.unsplash.com/premium_photo-1673792686302-7555a74de717?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Z2lybHxlbnwwfHwwfHx8MA%3D%3D"
-      />
+      <Greeting title="Encontre" isAuthenticated={!!user?.id} userImage="" />
       <div className="my-4">
         <TextInput
           placeholder="Pesquise receitas..."
